2616f21311bd04b6bd57335c5493a3bc24f132bc
Evgeniy Nechetov <evgeniy.nechetov@gmail.com>
2022-06-17 11:48:42 +0300

    add router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import { Provider } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import { Events } from './components/Events';
+
 import store from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <p>Hello!</p>
+      <Routes>
+        <Route path="/" element={<Events />} />
+      </Routes>
     </Provider>
   );
 }
--- frontend/src/store/reducers/root.reducer.js
+++ frontend/src/store/reducers/root.reducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import userReducer from './player.reducer';
+import userReducer from './user.reducer';
 import eventReducer from './event.reducer';
 
 const rootReducer = combineReducers({
