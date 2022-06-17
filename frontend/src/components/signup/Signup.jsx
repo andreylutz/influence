a15e63a3d904b81a61e363db14404ced6c19f2fd
Андрей <102368297+andreylutz@users.noreply.github.com>
2022-06-17 21:41:24 +0300

    registration-select done

--- frontend/src/components/signup/Signup.jsx
+++ frontend/src/components/signup/Signup.jsx
@@ -6,36 +6,30 @@ const Signup = () => {
   const [show, setShow] = useState(false);
   const inputtwo = useRef();
   const inputthree = useRef();
-  const inputforComp = useRef();
-  const inputforUser = useRef();
+  const inputfor = useRef();
 
 
-  const handleClose = () => {
-    console.log(inputforComp.current.value);
+  const handleClose = (event) => {
+    event.preventDefault();
 
-  //   async function fetchUserData() {
-  //     const response = await fetch('http://localhost:4000/api/auth', {
-  //       method: 'POST',
-  //       headers: { 'Content-Type': 'application/json' },
-  //       credentials: 'include',
-  //       body: JSON.stringify({
-  //         userEmail: inputtwo.current.value,
-  //         userPassword: inputthree.current.value,
-  //         role: inputfor.current.value,
-  //       }),
+    async function fetchUserData() {
+      const response = await fetch('http://localhost:4000/api/auth', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
+        body: JSON.stringify({
+          userEmail: inputtwo.current.value,
+          userPassword: inputthree.current.value,
+          role: inputfor.current.value,
+        }),
   
-  //     });
-  //     const text = await response.json();
-  //     console.log(text);
-  //   }
-  //   fetchUserData()
-
-
-    // window.location.assign('/')
-
-    // console.log(text);
-
-    // setShow(false);
+      });
+      const text = await response.json();
+      console.log(text);
+    }
+    fetchUserData()
+    window.location.assign('/')
+    setShow(false);
   };
 
   return (
@@ -49,9 +43,11 @@ const Signup = () => {
       <div className="signup-body">    
         <input ref={inputtwo} type='text' className='pols' name='userEmail' placeholder='Введите e-mail'/>
         <input ref={inputthree} type='password' className='pols' name='userPassword' placeholder='Придумайте пароль'/>
-        {/* <input ref={inputthree} type='password' className='pols' name='userPasswordRepeat' placeholder='Подтвердите пароль'/> */}
-        <label htmlFor="contactChoice1"><input ref={inputforComp} type="radio" id="contactChoice1" name="company" value="company" defaultChecked/>Компания</label>
-        <label htmlFor="contactChoice2"><input ref={inputforUser} type="radio" id="contactChoice2" name="user" value="user" defaultChecked/>Пользователь</label>
+        <input ref={inputthree} type='password' className='pols' name='userPasswordRepeat' placeholder='Подтвердите пароль'/>
+        <select ref={inputfor} className='selectorius' name="role">
+          <option value="user">Ваш статус в проекте USER</option>
+          <option value="company">Ваш статус в проекте COMPANY</option>
+       </select>
         <button onClick={handleClose} type='button' className='click' name='authBatton'>Зарегистрироваться</button>
       </div>
     </div>
