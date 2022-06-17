5c6b8e4ba8ce595479b421edc0434ab730be5e9b
Nikita <nikita.gav96@gmail.com>
2022-06-17 15:21:55 +0300

    navBar is ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import { Provider } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
+
 import { Events } from './components/Events';
+import NavBar from './components/nav/NavBar';
+import SideBar from './components/nav/SideBar';
 
 import store from './store';
 
 function App() {
   return (
     <Provider store={store}>
+      <NavBar/>
+      <SideBar/>
       <Routes>
         <Route path="/" element={<Events />} />
       </Routes>
