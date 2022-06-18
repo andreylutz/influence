fe2722d92840e2484a40c69914413e3cef83de17
stsoy01 <stsoy01@gmail.com>
2022-06-18 12:04:08 +0300

    changed events

--- frontend/src/components/UserProfile/UserEvents.jsx
+++ frontend/src/components/UserProfile/UserEvents.jsx
@@ -8,7 +8,7 @@ export default function UserEvents() {
   const events = [{ name: 'Delmar', location: 'Spain', picture: '...', date: '12.05.2022' }, { name: 'Creep', location: 'New York', picture: '...', date: '01.01.2021' }, { name: 'LA', location: 'Japan', picture: '...', date: '31.08.2022' }]
   return (
     <div className='userProfile__container'>
-      <ul style={{ display: 'flex' }}>
+      <ul className='userEvents__list'>
         {events.map((el) => <EventItem el={el} />)}
       </ul>
     </div>
