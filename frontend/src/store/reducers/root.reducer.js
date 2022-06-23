48070388f1a27270da59febf26d1d2638cf2d8c4
Evgeniy Nechetov <evgeniy.nechetov@gmail.com>
2022-06-23 11:43:49 +0300

    add theme

--- frontend/src/store/reducers/root.reducer.js
+++ frontend/src/store/reducers/root.reducer.js
@@ -3,15 +3,15 @@ import userReducer from './user.reducer';
 import eventReducer from './event.reducer';
 
 import myEventReducer from './myEvent.reducer';
-
+import themeReducer from '../reducers/theme.reducer';
 import userSettingsReducer from './userSettings.reducer';
-
 
 const rootReducer = combineReducers({
   user: userReducer,
   event: eventReducer,
   myEvent: myEventReducer,
   settings: userSettingsReducer,
+  theme: themeReducer,
 });
 
 export default rootReducer;
